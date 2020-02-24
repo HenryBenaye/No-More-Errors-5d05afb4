f357e87382447e1505714f181d48668492c6ad17
HENRY BENAYE <2106747@talnet.nl>
2020-02-24 15:54:06 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,13 +1,13 @@
 <?php
 $input = $argv[1];
 try{
-    if(!isset($input)){
+    if(!isset($input)) {
         throw new Exception("Je hebt geen bedrag meegegeven dat omgewisseld dient te worden");
-        }
-    elseif(!is_numeric($input)){
+    }
+    elseif(!is_numeric($input)) {
         throw new Exception("Voer een getal in");
     }
-    elseif($input < 0){
+    elseif($input < 0) {
         throw new Exception("Ik kan geen negatief bedrag wisselen");
     }
 
@@ -36,25 +36,25 @@ try{
         foreach($EUROS as $euro => $type1){
     
             $heleEuros = round($heleEuros,2);
-            if(floor($heleEuros / $euro) > 0){
+            if(floor($heleEuros / $euro) > 0) {
                 $times =  floor($heleEuros / $euro);
-                echo "$times X $euro" ."euro". PHP_EOL;
+                echo '$times X $euro' .'euro'. PHP_EOL;
                 $heleEuros = $heleEuros - ($times * $euro);
-            }
         }
+    }
 
-        foreach ($CENTEN as $cent => $type2) {
-            $heleCenten = round($heleCenten,2);
-            if(floor($heleCenten/ $cent) > 0) {
-                $times =  floor($heleCenten / $cent);
-                echo "$times X $cent" . "cent".PHP_EOL;
-                $heleCenten = $heleCenten - ($times * $cent);
+    foreach ($CENTEN as $cent => $type2) {
+            $heleCenten = round($heleCenten, 2);
+        if(floor($heleCenten/ $cent) > 0) {
+            $times =  floor($heleCenten / $cent);
+            echo '$times X $cent'. 'cent'.PHP_EOL;
+            $heleCenten = $heleCenten - ($times * $cent);
             }
-        }
+    }
 
    
 }
-catch(Exception $e){
+catch(Exception $e) {
     echo $e->getMessage();
 }
 
