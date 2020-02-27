e300987c723ddc769f085d64dd3b638017ea74fc
HENRY BENAYE <2106747@talnet.nl>
2020-02-27 13:37:04 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -40,16 +40,16 @@ try{
             $heleEuros = round($heleEuros, 2);
         if(floor($heleEuros / $euro) > 0) {
             $times =  floor($heleEuros / $euro);
-            echo '$times X $euro' .'euro'. PHP_EOL;
+            echo '$times X $euro euro'. PHP_EOL;
             $heleEuros = $heleEuros - ($times * $euro);
-                }
-         }
+            }
+    }
 
     foreach ($CENTEN as $cent => $type2) {
             $heleCenten = round($heleCenten, 2);
         if(floor($heleCenten/ $cent) > 0) {
             $times =  floor($heleCenten / $cent);
-            echo '$times X $cent'. 'cent'.PHP_EOL;
+            echo '$times X $cent cent'.PHP_EOL;
             $heleCenten = $heleCenten - ($times * $cent);
         }
     }
